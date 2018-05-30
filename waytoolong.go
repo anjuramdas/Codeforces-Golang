b22d93fc606c12f019000de96e4847c4b760c0b9
Anju Ramdas <ramadas.marangattu@gmail.com>
2018-05-31 00:39:00 +0530

    Update waytoolong.go

--- waytoolong.go
+++ waytoolong.go
@@ -8,7 +8,7 @@ import(
 
 func main(){
 	var n int
-	var word,l,out string
+	var word, l, out string
 	fmt.Scan(&n)
 	for i := 0; i < n; i++ {
 		fmt.Scan(&word)
@@ -18,9 +18,9 @@ func main(){
 		} else {
 			var output []string
 			l = strconv.FormatInt(int64(length-2), 10)
-			output = append(output,word[:1])
-			output = append(output,l)
-			output = append(output,word[length-1:length])
+			output = append(output, word[:1])
+			output = append(output, l)
+			output = append(output, word[length-1:length])
 			out = strings.Join(output, "")
 			fmt.Println(out)
 		 }
