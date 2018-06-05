425562a8178619a544ca1e65196567d43d06e766
Anju Ramdas <ramadas.marangattu@gmail.com>
2018-06-05 09:34:19 +0530

    commit

--- expression.go
+++ expression.go
@@ -11,15 +11,15 @@ func main() {
 	res4 := (a + b) * c
 	res5 := a + b + c
 	if res1 >= res2 && res1 >= res3 && res1 >= res4 && res1 >= res5 {
-	 fmt.Println(res1)
+	    fmt.Println(res1)
 	} else if res2 >= res1 && res2 >= res3 && res2 >= res4 && res2 >= res5 {
-		fmt.Println(res2) 
-	  } else if res3 >= res1 && res3 >= res2 && res3 >= res4 && res3 >= res5 {
-	  	  fmt.Println(res3) 
-	  	} else if res4 >= res1 && res4 >= res2 && res4 >= res3 && res4 >= res5 {
-	  	    fmt.Println(res4)
-	  	  } else {
-	  		  fmt.Println(res5)
-	  	    }
+	    fmt.Println(res2) 
+	} else if res3 >= res1 && res3 >= res2 && res3 >= res4 && res3 >= res5 {
+	    fmt.Println(res3) 
+	} else if res4 >= res1 && res4 >= res2 && res4 >= res3 && res4 >= res5 {
+	    fmt.Println(res4)
+	} else {
+	    fmt.Println(res5)
+	}
 }
 
