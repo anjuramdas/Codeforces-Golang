35a6769950bb6d910b7f9eef2f174c7fd5dc53f7
Anju Ramdas <ramadas.marangattu@gmail.com>
2018-06-07 21:04:46 +0530

    commit

---
+++ watermelon.go
@@ -0,0 +1,17 @@
+package main
+import (
+	"fmt"
+)
+func main() {
+	var weight int
+	fmt.Scan(&weight)
+	if weight % 2 == 0 {
+		if weight == 2 {
+			fmt.Println("NO")
+		} else {
+			fmt.Println("YES")
+		}
+		} else {
+			fmt.Println("NO")
+		}
+}
